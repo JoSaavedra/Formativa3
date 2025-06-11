51d298b8d4f39799d585fddf43ddad072e3a90a3
JoSaavedra <166625774+JoSaavedra@users.noreply.github.com>
2025-06-11 09:24:25 -0400

    Agregado proyecto creado a través de SpringInitial
    Fue creado con las dependencias del 2do mensaje en Discord

---
+++ Formativa3/src/main/java/com/example/Formativa3/Formativa3Application.java
@@ -0,0 +1,13 @@
+package com.example.Formativa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formativa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formativa3Application.class, args);
+	}
+
+}
